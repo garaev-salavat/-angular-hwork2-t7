735d206c5d10e22efd7710af12e658b1dfddbb41
Salavat <garaev.salavat@gmail.com>
2021-02-11 16:29:43 +0300

    commit1

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-hwork2-t7';
+  closeResult = '';
+  dateOfBirth:string;
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;
+        this.dateOfBirth = content;
+    }, 
+      (reason) => {this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;}
+      
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+}
